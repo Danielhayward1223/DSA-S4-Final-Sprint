2e57c8e95dbb503851ebdf50e94cc7804fa73d5a
Danielhayward1223 <daniel.hayward@keyin.com>
2024-08-07 00:36:18 -0230

    Progress

---
+++ src/main/java/com/keyin/BinaryTree/BinaryTree.java
@@ -0,0 +1,15 @@
+package com.keyin.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class BinaryTree {
+    public Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+}
---
+++ src/main/java/com/keyin/BinaryTree/Node.java
@@ -0,0 +1,8 @@
+package com.keyin.BinaryTree;
+
+public class Node {
+    public String value;
+    public Node leftNode;
+    public Node rightNode;
+    public int height;
+}
